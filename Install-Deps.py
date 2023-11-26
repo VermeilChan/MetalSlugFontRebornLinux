9aaa5f718cfb8f332a6797540ad3d699faa4c9c6
Vermeil <120065660+VermeilChan@users.noreply.github.com>
2023-11-26 02:59:04 +0100

    Fixed?

--- Install-Deps.py
+++ Install-Deps.py
@@ -27,11 +27,15 @@ def install_python_pip_venv(package_manager):
     print(f"Installing Python, pip, and venv using {package_manager}...")
     run_command(["sudo", package_manager, "install", "-y", "python3", "python3-pip"])
 
+def install_virtualenv():
+    print("Installing virtualenv...")
+    run_command(["pip", "install", "--no-cache-dir", "virtualenv"])
+
 def create_virtual_environment():
     print("Creating and activating virtual environment...")
     run_command(["python3", "-m", "venv", "venv"])
     activate_script = os.path.join("venv", "bin", "activate")
-    run_command([activate_script])
+    run_command(["source", activate_script])
 
 def install_packages_from_requirements_txt():
     print("Installing packages from requirements.txt...")
@@ -49,6 +53,7 @@ def main():
 
     update_packages(package_manager)
     install_python_pip_venv(package_manager)
+    install_virtualenv()
 
     create_virtual_environment()
     install_packages_from_requirements_txt()
