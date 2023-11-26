b98b20688493297791862668eab77e7c6df16043
Vermeil <120065660+VermeilChan@users.noreply.github.com>
2023-11-26 03:54:43 +0100

    WA

--- Install-Deps.sh
+++ Install-Deps.sh
@@ -43,3 +43,21 @@ echo "Virtual environment created and activated."
 echo "Packages installed from requirements.txt."
 echo "Virtual environment deactivated."
 echo "----------------------------------------"
+
+echo "Choose a version to run:"
+echo "1. Windowed Version"
+echo "2. Console Version"
+
+read -p "Enter your choice (1 or 2): " choice
+
+case $choice in
+    1)
+        python3 Src/windowed-version.py
+        ;;
+    2)
+        python3 Src/console-version.py
+        ;;
+    *)
+        echo "Invalid choice. Exiting."
+        ;;
+esac
