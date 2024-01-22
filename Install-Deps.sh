be7eaf9db9f2a064605232a109574038dbdf781b
Vermeil <120065660+VermeilChan@users.noreply.github.com>
2024-01-22 02:45:27 +0100

    WIP

--- Install-Deps.sh
+++ Install-Deps.sh
@@ -1,29 +1,26 @@
 #!/bin/bash
 
+echo "Welcome to the MetalSlugFontReborn Setup Script."
 echo "This script will download the required dependencies to use MetalSlugFontReborn."
-echo "It will automatically download Python 3, pip, venv, and optionally, xcb for GUI support."
+
+read -p "Do you want to continue with the setup? (y/n): " install_dependencies
 echo
 
-read -p "Do you want to continue? (y/n): " install_dependencies
-echo
-
-if [ "$install_dependencies" != "y" ]; then
-    echo "Exiting script. No dependencies will be installed."
+if [[ "$install_dependencies" != "y" ]]; then
+    echo "Exiting setup. No dependencies will be installed."
     exit 0
 fi
 echo
 
-read -p "Do you want to use the GUI version? (y/n): " install_xcb_libraries
+read -p "Do you want to install the GUI version? (y/n): " install_xcb_libraries
 echo
 
-if [ "$install_xcb_libraries" != "y" ]; then
-    echo "Skipping installation of libraries for GUI support."
-else
+if [[ "$install_xcb_libraries" == "y" ]]; then
     read -p "Enter your package manager (apt/dnf/pacman/zypper): " package_manager
     echo
 
     case $package_manager in
-        apt | dnf | pacman | zypper)
+        apt|dnf|pacman|zypper)
             ;;
         *)
             echo "Error: Invalid package manager. Please enter one of the following: apt, dnf, pacman, zypper."
@@ -33,64 +30,58 @@ else
     echo
 
     echo "Please wait, downloading dependencies..."
-
-    case $package_manager in
-        apt)
-            sudo apt update -y
-            ;;
-        dnf)
-            sudo dnf update -y
-            ;;
-        pacman)
-            sudo pacman -Syu --noconfirm
-            ;;
-        zypper)
-            sudo zypper update -y
-            ;;
-    esac
+    sudo "${package_manager}" update -y
 
     echo "Installing necessary libraries for GUI support..."
     case $package_manager in
         apt)
-            sudo apt install libxcb-cursor0 -y
+            sudo "${package_manager}" install libxcb-cursor0 -y
             ;;
         dnf)
-            sudo dnf install xcb-util-cursor -y
+            sudo "${package_manager}" install xcb-util-cursor -y
             ;;
         pacman)
-            sudo pacman -S xcb-util-cursor --noconfirm
+            sudo "${package_manager}" -S xcb-util-cursor --noconfirm
             ;;
         zypper)
-            sudo zypper install -y libxcb-cursor0
+            sudo "${package_manager}" install -y libxcb-cursor0
             ;;
     esac
 fi
 
 echo "Installing Python 3, pip, and venv..."
 case $package_manager in
-    apt | dnf)
-        sudo $package_manager install python3 python3-pip python3-venv -y
+    apt|dnf)
+        sudo "${package_manager}" install python3 python3-pip python3-venv -y
         ;;
     pacman)
-        sudo $package_manager -S python python-pip python-virtualenv --noconfirm
+        sudo "${package_manager}" -S python python-pip python-virtualenv --noconfirm
         ;;
     zypper)
-        sudo $package_manager install -y python3 python3-pip python3-virtualenv
+        sudo "${package_manager}" install -y python3 python3-pip python3-virtualenv
+        ;;
+    *)
+        echo "Error: Unsupported package manager. Please install Python 3, pip, and venv manually."
+        exit 1
         ;;
 esac
 
-echo "Creating and activating virtual environment..."
+echo "Creating and activating a virtual environment..."
 python3 -m venv metalslugfontreborn
 source metalslugfontreborn/bin/activate
 
 echo "Installing Python packages from requirements.txt..."
 pip install -r requirements.txt
 
-echo "Deactivating virtual environment..."
+echo "Deactivating the virtual environment..."
 deactivate
 
-echo "|-----------------------------------------|"
-echo "| Now, please run the following command:  |"
-echo "|             bash Run.sh                 |"
-echo "|-----------------------------------------|"
-echo
+cat <<EOF
+
+|-----------------------------------------|
+|     Setup completed successfully.       |
+| Now, please run the following command:  |
+|             bash Run.sh                 |
+|-----------------------------------------|
+
+EOF
--- Run.sh
+++ Run.sh
@@ -1,35 +1,49 @@
 #!/bin/bash
 
-source metalslugfontreborn/bin/activate
+METALSLUG_ENV="metalslugfontreborn"
 
-echo "Choose a version to run:"
-echo "1. Windowed Version"
-echo "2. Console Version"
+show_menu() {
+    echo "Choose a version to run:"
+    echo "1. Windowed Version"
+    echo "2. Console Version"
+    echo "3. Exit"
+}
 
-read -p "Enter your choice (1 or 2): " choice
+run_version() {
+    case $1 in
+        1)
+            python3 Src/windowed-version.py
+            ;;
+        2)
+            python3 Src/console-version.py
+            ;;
+        3)
+            echo "Exiting."
+            deactivate
+            exit 0
+            ;;
+        *)
+            echo "Invalid choice. Please try again."
+            ;;
+    esac
+}
 
-case $choice in
-    1)
-        python3 Src/windowed-version.py
-        ;;
-    2)
-        python3 Src/console-version.py
-        ;;
-    *)
-        echo "Invalid choice. Exiting."
-        deactivate
-        exit 1
-        ;;
-esac
+source $METALSLUG_ENV/bin/activate
 
-if [ $? -ne 0 ]; then
-    echo ""
-    echo "If the GUI version isn't working for you, even after installing xcb."
-    echo "please refer to the discussions on GitHub at https://github.com/NVlabs/instant-ngp/discussions/300."
-    echo ""
-    echo "Or, check if a new Qt6 update has been released."
-    echo "If so, you may need to wait until PyQt6 updates or use the CLI version."
-    echo ""
-fi
+while true; do
+    show_menu
 
-deactivate
+    read -p "Enter your choice (1, 2, or 3 to exit): " choice
+
+    run_version $choice
+
+    if [ $? -ne 0 ]; then
+        echo ""
+        echo "If the GUI version isn't working for you, even after installing xcb."
+        echo "Please refer to the discussions on GitHub at https://github.com/NVlabs/instant-ngp/discussions/300."
+        echo ""
+        echo "Or, check if a new Qt6 update has been released."
+        echo "If so, you may need to wait until PyQt6 updates or use the CLI version."
+        echo ""
+    fi
+done
